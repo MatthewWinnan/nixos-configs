e28cac571608245f5f4e49b626be0343db0c65eb
Matthew <mcwinnan@gmail.com>
2024-09-16 22:31:10 +0200

    Added some extra LSP configs

---
+++ nix/modules/applications/nixvim/plugins/lsp/lsp.nix
@@ -0,0 +1,115 @@
+{ config, pkgs, ... }: {
+  programs.nixvim.plugins = {
+
+    treesitter = {
+      enable = true;
+      nixvimInjections = true;
+      folding = true;
+
+      # Now in plugins.treesitter.settings.indent.enable
+      settings = {
+      indent.enable = true;
+      };
+    };
+
+    treesitter-refactor = {
+      enable = true;
+      highlightDefinitions = {
+        enable = true;
+        # Set to false if you have an `updatetime` of ~100.
+        clearOnCursorMove = false;
+      };
+    };
+
+    hmts.enable = true;
+
+    lsp = {
+      enable = true;
+      servers = {
+        # We use the updated version
+        nil-ls.enable = true;
+        html.enable = true;
+        jsonls.enable = true;
+        nginx-language-server.enable = true;
+        pylsp.enable = true;
+      };
+
+       keymaps = {
+        silent = true;
+        lspBuf = {
+          gd = {
+            action = "definition";
+            desc = "Goto Definition";
+          };
+          gr = {
+            action = "references";
+            desc = "Goto References";
+          };
+          gD = {
+            action = "declaration";
+            desc = "Goto Declaration";
+          };
+          gI = {
+            action = "implementation";
+            desc = "Goto Implementation";
+          };
+          gT = {
+            action = "type_definition";
+            desc = "Type Definition";
+          };
+          K = {
+            action = "hover";
+            desc = "Hover";
+          };
+          "<leader>cw" = {
+            action = "workspace_symbol";
+            desc = "Workspace Symbol";
+          };
+          "<leader>cr" = {
+            action = "rename";
+            desc = "Rename";
+          };
+        };
+        diagnostic = {
+          "<leader>cd" = {
+            action = "open_float";
+            desc = "Line Diagnostics";
+          };
+          "[d" = {
+            action = "goto_next";
+            desc = "Next Diagnostic";
+          };
+          "]d" = {
+            action = "goto_prev";
+            desc = "Previous Diagnostic";
+          };
+        };
+      };
+    };
+  };
+
+programs.nixvim.extraConfigLua = ''
+    local _border = "rounded"
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+      vim.lsp.handlers.hover, {
+        border = _border
+      }
+    )
+
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+      vim.lsp.handlers.signature_help, {
+        border = _border
+      }
+    )
+
+    vim.diagnostic.config{
+      float={border=_border}
+    };
+
+    require('lspconfig.ui.windows').default_options = {
+      border = _border
+    }
+  '';
+
+}
