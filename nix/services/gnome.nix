27171620944868a85dc09f48d716414d0cd589b9
Matthew <mcwinnan@gmail.com>
2024-09-18 22:36:29 +0200

    Fixes for vscode.
    - Pinned all extensions to supported vscode version. Not the cleanest
    but I am no longer getting unstable builds.
    - Removed the fhs package from vscode so I can have my immutability back
    (hopefully, untested seems to remember and take config changes).
    - Added general misc things such as keyring support for GNU, hopefully
    it'll help with future packages. This fixed to popups menus not showing
    in vscode.

--- nix/services/gnome.nix
+++ nix/services/gnome.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   config = {
     services = {
+      # This is actually super important for vscode, managed to get the popup screens to work after adding this.
       udev.packages = [pkgs.gnome.gnome-settings-daemon];
       gnome = {
         # Whether to enable gnome-keyring. This is usually necessary for storing
