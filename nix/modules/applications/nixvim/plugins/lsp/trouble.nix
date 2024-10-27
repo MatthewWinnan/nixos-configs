62f15514bbc7d8ecd8326131beeaf02291884d60
matthew <mcwinnan@gmail.com>
2024-10-27 16:43:32 +0200

    Added trouble keybindings

--- nix/modules/applications/nixvim/plugins/lsp/trouble.nix
+++ nix/modules/applications/nixvim/plugins/lsp/trouble.nix
@@ -1,5 +1,63 @@
+# DOCS -> hTrouble diagnostics togglettps://github.com/folke/trouble.nvim
 {
   programs.nixvim.plugins.trouble = {
     enable = true;
   };
+
+  programs.nixvim.keymaps = [
+    {
+      mode = "n";
+      key = "<leader>dx";
+      action = "<cmd>Trouble diagnostics toggle<cr>";
+      options = {
+        desc = "Diagnostics (Trouble)";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>dX";
+      action = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
+      options = {
+        desc = "Buffer Diagnostics (Trouble)";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>ds";
+      action = "<cmd>Trouble symbols toggle focus=false<cr>";
+      options = {
+        desc = "Symbols (Trouble)";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>dl";
+      action = "<cmd>Trouble lsp toggle focus=false win.position=left<cr>";
+      options = {
+        desc = "LSP Definitions / references / ... (Trouble)";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>dL";
+      action = "<cmd>Trouble loclist toggle<cr>";
+      options = {
+        desc = "Location List (Trouble)";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>dq";
+      action = "<cmd>Trouble qflist toggle<cr>";
+      options = {
+        desc = "Quickfix List (Trouble)";
+      };
+    }
+  ];
+
 }
