170beafa970e910e0294e7344a3f5c2d79cff834
MatthewWinnan <71087625+MatthewWinnan@users.noreply.github.com>
2025-04-20 16:47:07 +0200

    General formatting.

--- nix/services/printing.nix
+++ nix/services/printing.nix
@@ -1,9 +1,7 @@
 # Following advice from https://discourse.nixos.org/t/newly-announced-vulnerabilities-in-cups/52771
 # In general I am using the patch https://github.com/NixOS/nixpkgs/pull/344748 so should be fine
 # Other suggestions https://discourse.nixos.org/t/cups-cups-filters-and-libppd-security-issues/52780
-{inputs, ...}:
-{
-
+{inputs, ...}: {
   services.printing = {
     enable = false;
     startWhenNeeded = false;
--- shells/default.nix
+++ shells/default.nix
@@ -2,8 +2,7 @@
   system,
   inputs,
   ...
-}:
-let
+}: let
   # Pkgs impots
   pkgs = inputs.nixpkgs.legacyPackages.${system};
   pkgs_stable = inputs.nixpkgs-stable.legacyPackages.${system};
@@ -13,8 +12,7 @@ let
   zephyr = inputs.zephyr-nix.packages.${system};
 
   inherit (pkgs) lib runCommand fetchgit;
-in
-{
+in {
   python = import ./python.nix {inherit pkgs;};
   taskit = import ./taskit_dev.nix {inherit pkgs;};
   arduino = import ./arduino.nix {inherit pkgs;};
@@ -22,13 +20,13 @@ in
   freecad = import ./freecad.nix {inherit pkgs;};
 
   # With NixOS 25.04 I am getting buffer overflow issues, and it does not support libstdcxx5, we pin it to pkgs_stable, which is guarenteed to work
-  zmk = import ./zephyr/zmk.nix {inherit system lib stdenvNoCC runCommand fetchgit zephyr;
-      pkgs_stable = pkgs_stable;
-};
+  zmk = import ./zephyr/zmk.nix {
+    inherit system lib stdenvNoCC runCommand fetchgit zephyr;
+    pkgs_stable = pkgs_stable;
+  };
   zmk_kyria = import ./zephyr/zmk_kyria.nix {
     inherit pkgs system lib stdenvNoCC runCommand fetchgit;
     zephyr = zephyr;
     pkgs_stable = pkgs_stable;
   };
-
 }
