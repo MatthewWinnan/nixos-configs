91f2c9927c0f29601a5afe30e8b15849c98279e4
MatthewWinnan <71087625+MatthewWinnan@users.noreply.github.com>
2025-04-20 16:46:38 +0200

    Added methods to stop exposing CUPS.

--- nix/services/default.nix
+++ nix/services/default.nix
@@ -10,6 +10,7 @@
     ./dbus.nix
     ./udisks2.nix
     ./blueman.nix
+    ./printing.nix
   ];
 
   # services.xserver.libinput.enable = true;
---
+++ nix/services/printing.nix
@@ -0,0 +1,16 @@
+# Following advice from https://discourse.nixos.org/t/newly-announced-vulnerabilities-in-cups/52771
+# In general I am using the patch https://github.com/NixOS/nixpkgs/pull/344748 so should be fine
+# Other suggestions https://discourse.nixos.org/t/cups-cups-filters-and-libppd-security-issues/52780
+{inputs, ...}:
+{
+
+  services.printing = {
+    enable = false;
+    startWhenNeeded = false;
+    browsing = false;
+    browsed.enable = false;
+  };
+  systemd.services = {
+    cups-browsed.enable = false;
+  };
+}
